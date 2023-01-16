6144c0fa7e2dfa6a2a96983c6420cc744a368030
Cahzello <rizkyrasya.m@gmail.com>
2023-01-16 17:03:21 +0700

    initial commit

---
+++ app/views/about/index.php
@@ -0,0 +1,11 @@
+<div class="container">
+
+    <div class="p-5 mb-4 bg-dark rounded-3 mt-4 text-light">
+        <div class="container-fluid py-5">
+            <h1 class="display-5 fw-bold">Halo, nama saya <?= $data['nama']; ?></h1>
+            <p class="col-md-8 fs-4">Saya adalah seorang <?= $data['pekerjaan']; ?> berumur <?= $data['umur']; ?></p>
+            <!-- <button class="btn btn-primary btn-lg" type="button">Example button</button> -->
+        </div>
+    </div>
+
+</div>
---
+++ app/views/home/index.php
@@ -0,0 +1,12 @@
+<div class="container">
+
+    <div class="p-5 mb-4 bg-dark rounded-3 mt-4 text-light">
+        <div class="container-fluid py-5">
+            <h1 class="display-5 fw-bold">Selamat Datang</h1>
+            <p class="col-md-8 fs-4">Halo, ini adalah website mahasiswa</p>
+            <a href="<?= BASEURL; ?>/mahasiswa"><button class="btn btn-primary btn-lg" type="button">Lihat Mahasiswa</button></a>
+        </div> 
+        
+    </div>
+
+</div>
