feb52b747845b0297af589025884fe3250baf25b
Cahzello <rizkyrasya.m@gmail.com>
2023-02-11 20:40:20 +0700

    database wrapper

--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -7,6 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= $data['judul']; ?> </title>
     <link rel="stylesheet" href="<?= BASEURL; ?>/css/bootstrap.css">
+    <link rel="shortcut icon" href="<?= BASEURL ?>/img/lisa.png" type="image/x-icon">
 </head>
 
 <body>
