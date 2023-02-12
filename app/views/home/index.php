e9e3270cc982abd831e7420eca26021e712cfb75
Cahzello <rizkyrasya.m@gmail.com>
2023-02-12 20:31:42 +0700

    ubah nama uni

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -3,7 +3,7 @@
     <div class="p-5 mb-4 bg-dark rounded-3 mt-4 text-light">
         <div class="container-fluid py-5">
             <h1 class="display-5 fw-bold">Selamat Datang</h1>
-            <p class="col-md-8 fs-4">Halo, ini adalah website Daftar Mahasiswa Kampus Cerai</p>
+            <p class="col-md-8 fs-4">Halo, ini adalah website Daftar Mahasiswa Universitas Masbro</p>
             <a href="<?= BASEURL; ?>/mahasiswa"><button class="btn btn-primary btn-lg" type="button">Lihat Mahasiswa</button></a>
         </div> 
         
