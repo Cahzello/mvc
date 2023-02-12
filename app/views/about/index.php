0104fddc8ad42a9a1ca6553c8e82023e4bc3db7e
Cahzello <rizkyrasya.m@gmail.com>
2023-02-12 17:47:35 +0700

    delete data

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -4,7 +4,7 @@
         <div class="container-fluid py-5">
             <h1 class="display-5 fw-bold">Halo, nama saya <?= $data['nama']; ?></h1>
             <p class="col-md-8 fs-4">Saya adalah seorang <?= $data['pekerjaan']; ?> berumur <?= $data['umur']; ?></p>
-            <!-- <button class="btn btn-primary btn-lg" type="button">Example button</button> -->
+            <p class="col-md-8 fs-4">Saya adalah pembuat website ini.</p>
         </div>
     </div>
 
